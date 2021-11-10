6a2ebc48c80d1d01f6531fbc2f465086aea82abe
Abdel Ben <benabdelhak1@hotmail.com>
2021-11-10 15:12:25 +0100

    updated politicians table

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_11_09_132345) do
 
   create_table "politicians", force: :cascade do |t|
     t.string "name"
+    t.string "img_url"
     t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
