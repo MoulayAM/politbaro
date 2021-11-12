17961c494166380bde29cbf0e11c72dd8b97edb8
Abdel Ben <benabdelhak1@hotmail.com>
2021-11-12 11:50:53 +0100

    auto generated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_131708) do
+ActiveRecord::Schema.define(version: 2021_11_09_132345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
