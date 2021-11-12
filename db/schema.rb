c8d485a36f4f66cab42b43ec47c6d945d85014be
abdellah mallouk <moulayam16@gmail.com>
2021-11-12 11:50:45 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_131708) do
+ActiveRecord::Schema.define(version: 2021_11_09_132345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "news", force: :cascade do |t|
     t.string "title"
-    t.string "news_url"
     t.bigint "politician_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
