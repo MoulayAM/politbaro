0cacdb0d7b5d152039d1978d903b2813d7a8333b
zakaria hatim <zakariahatim8@gmail.com>
2021-11-12 16:03:49 +0100

    AddComments

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -1,4 +1,4 @@
 class Politician < ApplicationRecord
-  has_many :news
+  has_many :comments
   has_many :opinions, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :politicians, only: [:index, :show, :create, :new] do
     resources :news, only: [:index, :create, :new]
+    resources :comments, only: :create
     resources :opinions, only: [:index,:create, :new]
   end
   resources :themes, only: [:create, :new]
