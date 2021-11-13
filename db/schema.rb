4161054d2fe6ba6ca8d1c2a3d28bbf3664f3ccb7
zakaria hatim <zakariahatim8@gmail.com>
2021-11-13 15:48:11 +0100

    edit schema comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_11_12_143503) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -37,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_11_12_143503) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -44,7 +42,6 @@ ActiveRecord::Schema.define(version: 2021_11_12_143503) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "politician_id"
     t.index ["politician_id"], name: "index_comments_on_politician_id"
-
   end
 
   create_table "news", force: :cascade do |t|
@@ -95,7 +92,6 @@ ActiveRecord::Schema.define(version: 2021_11_12_143503) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "politicians"
