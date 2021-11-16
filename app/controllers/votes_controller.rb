5f4d22bcf9d3fda06dad56d9d3b63aa38dcb3b42
abdellah mallouk <moulayam16@gmail.com>
2021-11-16 16:27:47 +0100

    survey view/controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,10 @@
 class VotesController < ApplicationController
   def create
+    @vote = Vote.new
+    @vote.agreed = params[:vote][:agreed]
+    @vote.user_id = current_user.id
+    @vote.survey_id = params[:vote][:survey_id]
+    @vote.save!
     puts params
     redirect_to surveys_path
   end
